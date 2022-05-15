20a2a3533fa18799f7e482b4e23c0a6e23d1d9c3
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-05-15 22:10:27 +0600

    added service components

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Service from '../Service/Service';
+import flouride from '../../../assets/images/fluoride.png'
+import cavity from '../../../assets/images/cavity.png'
+import whitening from '../../../assets/images/whitening.png'
 
 const Services = () => {
+    const services = [
+        { _id: 1, pic: flouride, title: 'Flouride Treatment' },
+        { _id: 2, pic: cavity, title: 'Cavity Filling' },
+        { _id: 3, pic: whitening, title: 'Teeth Whitening' }
+    ]
     return (
         <div className='my-28'>
             <div>
                 <h3 className='text-primary font-bold text-xl uppercase'>Our Services</h3>
                 <h2 className='text-4xl'>Services We Provide</h2>
             </div>
-            <div>
-                <Service></Service>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
+                {services.map(service => <Service key={service._id} service={service}></Service>)}
             </div>
         </div>
     );
